b4b5502460eba258f940c6de3d1f50bfcd493555
Oleg <olegolegovich@inbox.ru>
2022-10-04 18:13:02 +0300

    add Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+
+while (counter <= number)
+{
+    if(counter%2 == 0)
+    {
+        Console.Write($"{counter} ");
+    }
+    counter++;
+}
