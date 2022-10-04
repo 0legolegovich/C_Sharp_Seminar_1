33400cc762a9c2e107536eccc4872ea809a86d2a
Oleg <olegolegovich@inbox.ru>
2022-10-04 17:47:05 +0300

    add Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if(number%2 == 0)
+{
+    Console.WriteLine($"Вы ввели число {number}. Оно четное. ");
+}
+else
+{
+    Console.WriteLine($"Вы ввели число {number}. Оно нечетное. ");
+}
