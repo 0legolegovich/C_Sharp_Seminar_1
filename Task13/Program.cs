6a212d04acbfe9d7c10807a0e175dc5b2c1335bb
Oleg <olegolegovich@inbox.ru>
2022-10-08 15:52:54 +0300

    add Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите заданное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = default;
+if (number<100 )
+    Console.WriteLine("Третьей цифры у введенного числа нет!");
+else {
+    thirdDigit = (number/100)%10;
+Console.Write($"Третья цифра введенного числа: {thirdDigit} ");
+     }
