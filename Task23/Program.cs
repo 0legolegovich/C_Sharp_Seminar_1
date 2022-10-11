f0a5f2e2ba5286d490043ee10f25ad1c70dcec41
Oleg <olegolegovich@inbox.ru>
2022-10-12 00:30:50 +0300

    add Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Cube(number);
+
+void Cube (int num)
+{
+	int count = 1;
+	while (count <= num)
+	{
+		int cube = count * count * count;
+		Console.WriteLine($"{count}  |  {cube}");
+		count++;
+	}
+}
