51ba2169e43e23c31018c37e9ed658191b038a65
Oleg <olegolegovich@inbox.ru>
2022-10-17 07:35:34 +0300

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int result = SumOfDigits(number);
+Console.WriteLine($"Сумма цифр введенного числа: {result}");
+
+int SumOfDigits(int num)
+{
+int res = default;
+int currentDigit = default;
+
+while (num > 0)
+{
+    currentDigit = num%10;
+    num = num/10;
+    res = res + currentDigit;
+
+}
+return res;
+}
