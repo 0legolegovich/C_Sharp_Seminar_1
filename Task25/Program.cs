4ccd65cdb71d463b814202f8a8a96517da4d9963
Oleg <olegolegovich@inbox.ru>
2022-10-17 07:07:12 +0300

    add Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число, которое нужно возвести в степень: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Введите, в какую степень ввести число {number}: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+
+double result = RaiseToPower(number, power);
+Console.WriteLine($"Результат операции: {result}");
+
+double RaiseToPower(double num, int pow)
+{
+double res = 1;
+
+for (int i = 1; i <= pow; i++)
+    {
+    res = res * num;
+    }
+return res;
+}
+
+
