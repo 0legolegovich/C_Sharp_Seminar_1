172e3859fc0eee6253b2e6a0f5dcee61099d4b17
Oleg <olegolegovich@inbox.ru>
2022-10-23 19:26:30 +0300

    add Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Задайте количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[rows, columns];
+
+FillArray(matrix);
+PrintArray(matrix);
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(-100,100);
+            matr[i,j] = matr[i,j]/10;
+        }
+        Console.WriteLine();
+    }
+}
