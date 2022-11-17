024c131577e8122643f70252f07530ffe09bc4df
Oleg <olegolegovich@inbox.ru>
2022-11-17 10:20:39 +0300

    add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NumbersRecursion(1, n));
+
+string NumbersRecursion(int a, int b)
+{
+    if (a <= b) return NumbersRecursion(a + 1, b) + $"{a} ";
+    else return string.Empty;
+}
