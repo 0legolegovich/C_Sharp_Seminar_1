061306f483d4a30380e2e9709e1ebb2fa81a3335
Oleg <olegolegovich@inbox.ru>
2022-11-17 10:29:33 +0300

    add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumRecursion(m, n));
+
+int SumRecursion(int m, int n)
+{
+    if (m == n) return 0;
+    else if (m < n) return n + SumRecursion(m, n - 1);
+    else return n + SumRecursion(m - 1, n);
+    
+}
+
